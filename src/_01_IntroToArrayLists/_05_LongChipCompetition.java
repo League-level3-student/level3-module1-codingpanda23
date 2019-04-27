d7bea0119bbf3f9b61c0769d4586412e9f294a29
codingpanda23 <league@WTS20.attlocal.net>
2019-04-27 15:55:38 -0700

    RemovingStuffFromArrayLists; LongChipCompetition(IP)
    :sunglasses:

--- src/_01_IntroToArrayLists/_05_LongChipCompetition.java
+++ src/_01_IntroToArrayLists/_05_LongChipCompetition.java
@@ -12,11 +12,17 @@ public class _05_LongChipCompetition {
 	 * the longest chip. You may not edit the Chip or Beatle classes. Make sure to
 	 * initialize The Beatles before you start your search. *
 	 **/
-	private ArrayList<Beatle> theBeatles = new ArrayList<Beatle>();
+	private static ArrayList<Beatle> theBeatles = new ArrayList<Beatle>();
 
 	public static void main(String[] args) {
 		_05_LongChipCompetition lcc = new _05_LongChipCompetition();
 		
+		lcc.initializeBeatles();
+		for (int i = 0; i < theBeatles.size(); i++) {
+			Beatle bug = theBeatles.get(i);
+			ArrayList<Chip> fries = bug.getChips();
+			
+		}
 	}
 	
 	private void initializeBeatles() {
