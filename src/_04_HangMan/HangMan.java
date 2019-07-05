510dedeb3cfee2945e1951a39c48ab86373ef88d
League Student <league@iMac-18.attlocal.net>
2019-07-05 16:27:33 -0700

    HangMan(IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,7 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -7,31 +9,85 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan{
+public class HangMan implements KeyListener{
 	static Stack<String> s = new Stack<String>();
 	static JFrame frame = new JFrame();
 	static JPanel panel = new JPanel();
 	static JLabel label = new JLabel();
 	static String underscore = new String();
+	static int lives = 10;
+	static String word;
 	
 	public static void main(String[] args) {
+		HangMan h = new HangMan();
+		h.run();
+		
+	}
+	
+	void run() {
 		frame.add(panel);
 		panel.add(label);
 		frame.setVisible(true);
 		frame.pack();
-		
+		frame.addKeyListener(this);
+			
 		int words = Integer.parseInt(JOptionPane.showInputDialog("How many words do you want to spell?"));
 		for (int i = 0; i < words; i++) {
-			s.push(Utilities.readRandomLineFromFile("dictionary.txt"));
-			String next = new String();
-			if (!s.contains(next)) {
-				s.add(next);
-				i++;
+			String next = s.push(Utilities.readRandomLineFromFile("dictionary.txt"));			
+		}
+		while (!s.isEmpty()) {
+			word = s.pop();
+			//System.out.println(word);
+			for (int i = 0; i < word.length(); i++) {
 				underscore+="_";
 			}
 		}
+		label.setText(underscore + "\n Lives: " + lives);
 		
-		s.pop();
+		
+	}
+		
+		
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		//System.out.println(e.getKeyChar());
+		if (word.contains(e.getKeyChar()+"")) {
+			int w = word.indexOf(e.getKeyChar());
+			String newunderscore = "";
+			for (int i = 0; i < word.length(); i++) {
+				if (word.charAt(i) == e.getKeyChar()) {
+					newunderscore+=e.getKeyChar();
+				}else {
+					newunderscore+=underscore.charAt(i);
+				}
+			}
+			underscore = newunderscore;
+			label.setText(underscore);
+		}else {
+			lives--;
+		}
+		label.setText(underscore + "\n Lives: " + lives);
+		
+		if (!underscore.contains("_") && lives != 0) {
+			JOptionPane.showMessageDialog(null, "You've guessed the word!");
+			
+		}
+		else if (lives == 0) {
+			JOptionPane.showMessageDialog(null, "Game Over");
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
